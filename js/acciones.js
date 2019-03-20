a9542768a68ba8d90382c96356e7763956dcf8dc
pablo202002 <pablinskyprogg@gmail.com>
2019-03-20 12:00:23 -0600

    6subir

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,7 @@
 $(document).ready(function (e){
 	document.addEventListener ("deviceready",function(){
 		$('#dips table td').eq(3).text(device.model);
-		$('#dips table td').eq(4).text(device.cordova);
+		$('#dips table td').eq(5).text(device.cordova);
 		$('#dips table td').eq(7).text(device.platform);
 		$('#dips table td').eq(9).text(device.version);
 		$('#dips table td').eq(11).text(device.uuid);
